97a5148ba1c4fa027ab90163af6021498c38e6a2
Ahmad Haris Kurniawan <ahmadharisk@gmail.com>
2022-08-09 07:46:15 +0700

    penambahan menu

---
+++ data/DataMenu.js
@@ -0,0 +1,47 @@
+const dataMenu = [
+	{
+		id: 1,
+		nama: "PENGUMUMAN",
+		link: "/pengumuman",
+	},
+	{
+		id: 2,
+		nama: "Antrian Online",
+		link: "https://kunjung.pajak.go.id/",
+	},
+	{
+		id: 3,
+		nama: "Saluran Komunikasi Resmi 071",
+		link: "/saluran-komunikasi-resmi",
+	},
+	{
+		id: 4,
+		nama: "Survei Penilaian Layanan 071",
+		link: "/survei-penilaian-layanan",
+	},
+	{
+		id: 5,
+		nama: "Materi Kelas Pajak",
+		link: "/materi-kelas-pajak",
+	},
+	{
+		id: 6,
+		nama: "Perpanjangan Sertifikat Elektronik",
+		link: "/perpanjangan-sertifikat-elektronik",
+	},
+	{
+		id: 7,
+		nama: "Formulir Permohonan",
+		link: "/formulir-permohonan",
+	},
+	{
+		id: 8,
+		nama: "Aplikasi Perpajakan",
+		link: "/aplikasi-perpajakan",
+	},
+	{
+		id: 9,
+		nama: "Tutorial Pajak",
+		link: "/",
+	},
+]
